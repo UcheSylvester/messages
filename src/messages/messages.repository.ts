dbfe49399bbd18d2479073786c8e1629dae619df
UcheSylvester <okorocode@gmail.com>
2021-12-12 13:42:40 +0100

    Connecting the services and repository together

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -3,22 +3,16 @@ import { readFile, writeFile } from 'fs/promises';
 export class MessagesRepository {
   async findAll() {
     const contents = await readFile('messages.json', 'utf-8');
-    console.log({ contents });
-
     return JSON.parse(contents);
   }
 
   async findOne(id: string) {
-    console.log(id);
-
     const messages = await this.findAll();
 
     return messages[id];
   }
 
   async create(content: string) {
-    console.log(content);
-
     const messages = await this.findAll();
 
     const id = Math.floor(Math.random() * 999);
